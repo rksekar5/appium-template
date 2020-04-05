0035a8a2c37a1e0893fb902c4ce716bae6c24167
Parthiban <parthiban.rajasekaran@diconium.com>
2020-04-05 20:55:51 +0200

    refactor common native app utils

--- src/main/java/mobile/utils/mobileUtils.java
+++ src/main/java/mobile/utils/mobileUtils.java
@@ -1,5 +1,27 @@
 package mobile.utils;
 
+import static common.Logger.logInfo;
+
+import io.appium.java_client.MobileElement;
+
 public class mobileUtils {
+
+  public static String getMobileAttributeValue(MobileElement mobileElement){
+    return mobileElement.getAttribute("value");
+  }
+
+  public static void clickMobileElement(MobileElement mobileElement){
+    mobileElement.click();
+    logInfo(String.format("%s clicked successfully", mobileElement));
+  }
+
+  public static void sendKeysToMobileElement(MobileElement mobileElement, String input){
+    mobileElement.sendKeys(input);
+    logInfo(String.format("%s is entered in %s", input, mobileElement));
+  }
+
+  public static String getTextFromMobileElement(MobileElement mobileElement){
+    return mobileElement.getText();
+  }
 
 }
