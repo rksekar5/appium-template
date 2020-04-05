f092a2b302b46f272d7da012546dde8bb6782ec9
Parthiban <parthiban.rajasekaran@diconium.com>
2020-04-05 20:58:53 +0200

    add sample flutter test

--- src/test/java/mobile/androidapp/flutterdemo/FlutterDemoTest.java
+++ src/test/java/mobile/androidapp/flutterdemo/FlutterDemoTest.java
@@ -1,19 +1,32 @@
 package mobile.androidapp.flutterdemo;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import mobile.androidapp.common.BaseTest;
+import mobile.androidapp.common.FlutterBaseTest;
+import mobile.androidapp.flutter_app.page_objects.HomePage;
 import org.testng.annotations.Test;
 
-public class FlutterDemoTest extends BaseTest {
+public class FlutterDemoTest extends FlutterBaseTest {
 
   @Test
   public void flutterDemoTest() throws IOException, InterruptedException {
-    AndroidDriver<AndroidElement> driver = capabilities("flutter_demo_app");
-    Thread.sleep(10000);
+    AndroidDriver<MobileElement> driver = capabilities("flutter_demo_app");
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    HomePage homePage = new HomePage(driver);
+
+//    Assert.assertTrue(homePage.PageHeader.isDisplayed());
+//    System.out.println("Page header is displayed in the home page as expected");
+//    int initialCount = Integer.parseInt(homePage.ToolTipCounter.getText());
+//    System.out.println("Initial counter value is: "+initialCount);
+//    homePage.Increment.click();
+//    int postIncrementValue = Integer.parseInt(homePage.ToolTipCounter.getText());
+//    System.out.println("Post incremental value is: "+postIncrementValue);
+
+    homePage.StimulateHostCardButton.click();
+    Thread.sleep(10000);
 
   }
 }
