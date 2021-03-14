da096b925c1124025d4425354755ca803d03691b
Sekar Karuppiah <Sekar.Karuppiah@diconium.com>
2021-03-14 20:47:12 +0100

    revert emulator settings

--- src/main/java/mobile/driverhandler/IosFactory.java
+++ src/main/java/mobile/driverhandler/IosFactory.java
@@ -28,7 +28,6 @@ public class IosFactory extends AppFactory {
      *     capabilities.setCapability("UDID","00008030-000C353A0C3A802E");
      *     Provide the gitlab url where the app is present and it will pick automatically
      *     capabilities.setCapability("app","https://github.com/appium/appium/blob/master/sample-code/apps/TestApp.app.zip?raw=true");
-     *     capabilities.setCapability("app","https://github.com/appium/appium/blob/master/sample-code/apps/TestApp.app.zip?raw=true");
      */
     File appDirectory = new File("src/app");
     File app = new File(appDirectory, readValueFromMobileConfigFile(appName));
@@ -48,7 +47,11 @@ public class IosFactory extends AppFactory {
     capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 100);
     capabilities.setCapability("xcodeSigningId", "iPhone Developer");
     capabilities.setCapability("useNewWDA", false);
+    //capabilities.setCapability("app","https://github.com/appium/appium/blob/master/sample-code/apps/TestApp.app.zip?raw=true");
     capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+//    capabilities.setCapability("UDID","00008030-000C353A0C3A802E");
+//    capabilities.setCapability("bundleId", "com.volkswagen.oneapp.alpha");
+
     appiumDriver = new IOSDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
