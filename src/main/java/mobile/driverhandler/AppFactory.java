dab74273857eb0dd94d2b196daa26ee2b4a8ec9e
karupsek <sekar.karuppiah@diconium.com>
2021-07-22 17:11:28 +0200

    changed iphone 12

--- src/main/java/mobile/driverhandler/AppFactory.java
+++ src/main/java/mobile/driverhandler/AppFactory.java
@@ -16,42 +16,42 @@ import static mobile.common.ConfigReader.getValueFromJsonConfigFile;
 @Slf4j
 public class AppFactory {
 
-  public static AppiumDriverLocalService service;
-  public static AppiumDriver<MobileElement> appiumDriver;
+    public static AppiumDriverLocalService service;
+    public static AppiumDriver<MobileElement> appiumDriver;
 
-  public AppiumDriverLocalService startServer() {
-    AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder();
-    service = AppiumDriverLocalService.buildService(serviceBuilder);
-    boolean serverRunning = !stopServerOnPort(4723);
-    if (null != service && serverRunning) {
-      service.stop();
-      log.debug("Killing Appium service before starting a new session");
+    public AppiumDriverLocalService startServer() {
+        AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder();
+        service = AppiumDriverLocalService.buildService(serviceBuilder);
+        boolean serverRunning = !stopServerOnPort(4723);
+        if (null != service && serverRunning) {
+            service.stop();
+            log.debug("Killing Appium service before starting a new session");
+        }
+
+        service.start();
+        return service;
     }
 
-    service.start();
-    return service;
-  }
+    /**
+     * Try to close server on socket with provided port number.
+     *
+     * @param portNumber
+     * @return true if successful
+     */
+    private static boolean stopServerOnPort(int portNumber) {
 
-  /**
-   * Try to close server on socket with provided port number.
-   *
-   * @param portNumber
-   * @return true if successful
-   */
-  private static boolean stopServerOnPort(int portNumber) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            serverSocket.close();
+            return true;
 
-    try {
-      ServerSocket serverSocket = new ServerSocket(portNumber);
-      serverSocket.close();
-      return true;
-
-    } catch (IOException e) {
-      return false;
+        } catch (IOException e) {
+            return false;
+        }
     }
-  }
 
-  @SneakyThrows
-  public static String readValueFromMobileConfigFile(@NotNull String propName) {
-    return getValueFromJsonConfigFile("mobile_config.json", propName);
-  }
+    @SneakyThrows
+    public static String readValueFromMobileConfigFile(@NotNull String propName) {
+        return getValueFromJsonConfigFile("mobile_config.json", propName);
+    }
 }
