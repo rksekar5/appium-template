4b5714ea47fde0dbe109c2cd5a38be2db0c7fe8a
karupsek <sekar.karuppiah@diconium.com>
2021-07-26 13:04:12 +0200

    added screenshot capability changed testFailureIgnore=true in pom file

--- src/main/java/mobile/common/AppiumListener.java
+++ src/main/java/mobile/common/AppiumListener.java
@@ -1,15 +1,20 @@
 package mobile.common;
 
 import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import ru.yandex.qatools.allure.annotations.Attachment;
-import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.testng.AllureTestListener;
 
-import static mobile.browser.common.WebDriverHolder.getDriver;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static mobile.driverhandler.AppFactory.appiumDriver;
 
 public class AppiumListener extends AllureTestListener {
 
@@ -28,21 +33,6 @@ public class AppiumListener extends AllureTestListener {
     Logger.logInfo(tr.getName() + " \n--- SUCCESS ---\n");
   }
 
-  @Step("Step on Failure")
-  @Override
-  public void onTestFailure(ITestResult tr) {
-    Logger.logError(tr.getName() + " --- FAILED --- ");
-    // saveTextLog(tr.getName());
-    Logger.logInfo(String.format("%s failed and screenshot taken", tr.getName()));
-    saveScreenshotPNG();
-    Throwable ex = tr.getThrowable();
-    if (ex != null) {
-      String cause = ex.toString();
-      Logger.saveTextLog(cause);
-      Logger.logError(cause + "\n");
-    }
-  }
-
   @Override
   public void onTestSkipped(ITestResult tr) {
     Logger.logInfo(tr.getName() + " --- SKIPPED ---\n");
@@ -53,22 +43,57 @@ public class AppiumListener extends AllureTestListener {
     }
   }
 
-  /**
-   * Take and save a screen shot as an attachment to the allure report.
-   *
-   * @return the screenshot, which will be handled by the annotation
-   */
+  // This method will execute only on the event of fail test.
+  public void onTestFailure(ITestResult tr) {
+    captureScreenShot(tr, "fail");
+  }
+
+  // This method will execute before starting of Test suite.
+  private static boolean deleteDir(File dir) {
+    if (dir.isDirectory()) {
+      String[] children = dir.list();
+      assert children != null;
+      for (String aChildren : children) {
+        boolean success = deleteDir(new File(dir, aChildren));
+        if (!success) {
+          return false;
+        }
+      }
+    }
+    return dir.delete();
+  }
+
+  public void onTestFailedButWithinSuccessPercentage(ITestResult tr) {}
+  // Function to capture screenshot.
   @SneakyThrows
   @Attachment(value = "page screenshot", type = "image/png")
-  private static byte[] saveScreenshotPNG() {
-    Logger.logDebug("Taking screenshot");
-    /* Only used when we integrate Report Portal
-           Logger.logInfo(
-                   new ObjectMessage(
-                           new ReportPortalMessage(
-                                   ((TakesScreenshot)
-    getDriver()).getScreenshotAs(OutputType.FILE),
-                                   "Appending Screenshot")));*/
-    return ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+  private static byte[] captureScreenShot(ITestResult result, String status) {
+    String destDir = "";
+    String passfailMethod =
+        result.getMethod().getRealClass().getSimpleName()
+            + "."
+            + result.getMethod().getMethodName();
+    // To capture screenshot.
+    File scrFile = appiumDriver.getScreenshotAs(OutputType.FILE);
+    DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy__hh_mm_ssaa");
+    // If status = fail then set folder name "screenshots/Failures"
+    if (status.equalsIgnoreCase("fail")) {
+      destDir = "screenshots/Failures";
+    }
+    // If status = pass then set folder name "screenshots/Success"
+    else if (status.equalsIgnoreCase("pass")) {
+      destDir = "screenshots/Success";
+    }
+    // To create folder to store screenshots
+    new File(destDir).mkdirs();
+    // Set file name with combination of test class name + date time.
+    String destFile = passfailMethod + " - " + dateFormat.format(new Date()) + ".png";
+    try {
+      // Store file at destination folder location
+      FileUtils.copyFile(scrFile, new File(destDir + "/" + destFile));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return appiumDriver.getScreenshotAs(OutputType.BYTES);
   }
 }
